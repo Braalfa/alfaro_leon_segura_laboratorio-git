9632dd9b7a582220e0742cf86fd10bf8d33bd787
Bratan <alfabrayan12@gmail.com>
2021-08-18 22:00:30 -0600

    JIRA-121

--- prueba.py
+++ prueba.py
@@ -1,11 +1,15 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print("hola broders")
 =======
 <<<<<<< HEAD
-COPYRIGHT
 
 print("hola hermanos")
 
 =======
 def suma(x,y):
     print(x+y)
+=======
+
+print("holaFDSGFD hermanos")
+>>>>>>> Stashed changes
