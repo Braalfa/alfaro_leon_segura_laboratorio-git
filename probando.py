f342da39d21c36e9c133820db77005330cb7a01c
Tomás <tomas.seguram@gmail.com>
2021-08-18 22:08:41 -0600

    JIRA-2343

--- probando.py
+++ probando.py
@@ -1,19 +1,26 @@
 print("hola mundo") 
-print("hola mundo") 
+printcopyrcopyrigth
+copyrigthigth
+copyrigth("hola mundo") 
 
+print("hola mundo")
+print("hcopcopyrigth
+copyrigthyrigth
+copyrigthola mundo") 
+print("hola mundo") 
+copyrigth
+copyrigth
 print("hola mundo")
 print("hola mundo") 
 print("hola mundo") 
 
-print("hola mundo")
-print("hola mundo") 
-print("hola mundo") 
-
-print("hola mundo") 
+print("holacopyrigth
+copyrigth mundo") 
 copyrigth
 
 print("hola mundo")
-print("hola mundo") 
+print("holacopyrigth
+copyrigth mundo") 
 print("hola mundo") 
 
 print("hola mundo") 
